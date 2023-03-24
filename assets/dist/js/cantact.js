4d2f73f421be74de2e9f501ea188d5ddc067d1d1
Ngoc Quang Do <info.dongocquang@gmail.com>
2023-03-24 13:05:53 +0700

    upload source code

---
+++ assets/dist/js/cantact.js
@@ -0,0 +1,15 @@
+function sendInfo(){
+    hoten = $("#txtHoten").val();
+    email = $("#txtEmail").val();
+    phone = $("#txtSDT").val();
+    dvvt = $("#txtDvVt").val();
+    send = $("#txtSend").val();
+    content = $("#txtNoidung").val();
+
+    console.log(hoten);
+    console.log(email);
+    console.log(phone);
+    console.log(dvvt);
+    console.log(send);
+    console.log(content);
+}
